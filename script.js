eabef00a8f60e2243e51b8512ff597af8184bd47
Ojukwu-Valentine1933 <thejasperval@gmail.com>
2024-03-03 12:41:17 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdownButton = document.getElementById('dropdown-button');
+    const textAndButtonDiv = document.getElementById('text-and-button');
+
+    dropdownButton.addEventListener("click", function () {
+        // Toggle the visibility of the text-and-button div when the menu button is clicked
+        if (textAndButtonDiv.style.display === "block" || textAndButtonDiv.style.display === "") {
+            textAndButtonDiv.style.display = "none";
+        } else {
+            textAndButtonDiv.style.display = "block";
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll('.slideshow-container .slide');
+    let index = 0;
+
+    function showSlide() {
+        slides.forEach(slide => {
+            slide.classList.remove('active');
+        });
+
+        index = (index + 1) % slides.length;
+        slides[index].classList.add('active');
+
+        setTimeout(showSlide, 10000);
+    }
+
+    showSlide();
+});
+
+
+
+
+
+
+
